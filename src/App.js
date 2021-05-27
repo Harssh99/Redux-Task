b61320add1536f44a80ba29156d9eb6f6aa82a80
Harssh99 <500067787@stu.upes.ac.in>
2021-05-27 23:15:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Provider } from 'react-redux';
+import store from './redux/store'
+import './App.css';
+import Button from './Components/Button';
+import DispCount from './Components/DispCount';
+
+function App() {
+  return (
+      <Provider store= {store}>
+        <div className="App">
+          <div className="outerContainer" style={{float: 'left'}}>
+            <Button />
+          </div> 
+          <div className="outerContainer" style={{float: 'right'}}>
+            <DispCount />
+          </div>     
+        </div>
+    </Provider>
+  );
+}
+
+export default App;
